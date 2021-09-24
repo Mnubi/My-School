32b2120a2891c00f20f61643f083c3694af9ae33
kelicaleb <kelicaleb7@gmail.com>
2021-09-24 09:30:18 +0300

    main changes

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ b.addEventListener("submit", function(x){
     const jsform = new FormData(this);
     console.log(jsform.get("flexRadioDefault"))
     console.log(jsform.get("Default"))
-    if((jsform.get("flexRadioDefault")) && (jsform.get("Default"))(jsform.get("select"))){
+    if((jsform.get("flexRadioDefault")) && (jsform.get("Default")) && (jsform.get("select"))){
         alert("please go to the contact form to contact us ")
     }
     else{
